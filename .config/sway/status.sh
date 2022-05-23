e68e9a598beb5faad110fb28841dec2d59e6a6c5
Stefan Doe <stefan@stfnx.xyz>
2022-05-23 21:51:41 +0200

    [Fix] Show time in 24h format

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -2,6 +2,6 @@
 
 wifi="$(iwctl station wlan0 show | grep -i "connected network" | xargs | sed -e "s/^Connected network //")"
 batcap=$(cat /sys/class/power_supply/BAT0/capacity)
-date=$(date +'%d.%m.%Y %l:%M')
+date=$(date +'%d.%m.%Y %H:%M')
 
 echo "WIFI:${wifi}  BAT:${batcap}  ${date} "
