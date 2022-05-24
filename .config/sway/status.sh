d86f5a8a7a00a47eb85f8664a09664cdb43f2ded
Stefan Doe <stefan@stfnx.xyz>
2022-05-24 19:29:48 +0200

    [Add] Volume in status bar

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -4,4 +4,8 @@ wifi="$(iwctl station wlan0 show | grep -i "connected network" | xargs | sed -e 
 batcap=$(cat /sys/class/power_supply/BAT0/capacity)
 date=$(date +'%d.%m.%Y %H:%M')
 
-echo "WIFI:${wifi}  BAT:${batcap}  ${date} "
+volume=$(pacmd list-sinks | grep -A 15 '* index' | awk '/volume: front/{ print $5 }' | sed 's/[%|,]//g')
+muted=$( pacmd list-sinks | grep -A 15 '* index' | awk '/muted:/{ print $2 }')
+[ $muted = yes ] && volume="MUTED"
+
+echo "WIFI:${wifi}  BAT:${batcap}  VOL:${volume}  ${date} "
